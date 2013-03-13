13b57622681d86325277679b95392fb3446c38ea
herr.grossmann <herr.grossmann@gmx.de>
2013-03-13 15:00:14 +0000

    added initial tutorial4

---
+++ tutorial/tutorial4/modules/app/org.jowidgets.tutorials.tutorial4.app.service/src/main/java/org/jowidgets/tutorials/tutorial4/app/service/descriptor/PersonDtoDescriptorBuilder.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2013, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.tutorials.tutorial4.app.service.descriptor;
+
+import org.jowidgets.cap.common.api.bean.IBeanPropertyBluePrint;
+import org.jowidgets.cap.common.api.sort.Sort;
+import org.jowidgets.tutorials.tutorial4.app.common.bean.IPerson;
+
+public final class PersonDtoDescriptorBuilder extends AbstractDtoDescriptorBuilder {
+
+	public PersonDtoDescriptorBuilder() {
+		super(IPerson.class);
+
+		setLabelSingular("Person");
+		setLabelPlural("Persons");
+		setDefaultSorting(Sort.create(IPerson.NAME_PROPERTY));
+
+		setRenderingPattern("$" + IPerson.NAME_PROPERTY + "$");
+
+		//IBean.ID_PROPERTY
+		addIdProperty();
+
+		//IPerson.NAME_PROPERTY
+		IBeanPropertyBluePrint propertyBp = addProperty(IPerson.NAME_PROPERTY);
+		propertyBp.setLabel("Name");
+		propertyBp.setDescription("The name of the person");
+
+		//IPerson.DATE_OF_BIRTH_PROPERTY
+		propertyBp = addProperty(IPerson.DATE_OF_BIRTH_PROPERTY);
+		propertyBp.setLabel("Date of birth");
+		propertyBp.setDescription("The date when the person was born");
+
+		//IPerson.GENDER_PROPERTY
+		propertyBp = addProperty(IPerson.GENDER_PROPERTY);
+		propertyBp.setLabel("Gender");
+		propertyBp.setDescription("The persons gender");
+
+		//IBean.VERSION_PROPERTY
+		addVersionProperty();
+	}
+
+}
