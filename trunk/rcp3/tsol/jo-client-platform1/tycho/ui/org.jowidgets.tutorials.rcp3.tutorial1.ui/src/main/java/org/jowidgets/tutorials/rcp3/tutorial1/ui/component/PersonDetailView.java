16ce64b002ec88e1c98500fc984247d7fbfd34bd
herr.grossmann@gmx.de <herr.grossmann@gmx.de@aada74ba-025b-2414-2ef6-ebfe0e18f327>
2014-04-08 07:01:48 +0000

    added tsol folder

---
+++ trunk/rcp3/tsol/jo-client-platform1/tycho/ui/org.jowidgets.tutorials.rcp3.tutorial1.ui/src/main/java/org/jowidgets/tutorials/rcp3/tutorial1/ui/component/PersonDetailView.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2013, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.tutorials.rcp3.tutorial1.ui.component;
+
+import java.util.List;
+
+import org.jowidgets.addons.icons.silkicons.SilkIcons;
+import org.jowidgets.api.widgets.IContainer;
+import org.jowidgets.cap.ui.api.CapUiToolkit;
+import org.jowidgets.cap.ui.api.attribute.IAttribute;
+import org.jowidgets.cap.ui.api.table.IBeanTableModel;
+import org.jowidgets.cap.ui.api.widgets.IBeanSelectionFormBluePrint;
+import org.jowidgets.cap.ui.api.widgets.ICapApiBluePrintFactory;
+import org.jowidgets.cap.ui.tools.attribute.MetaAttributesFilter;
+import org.jowidgets.common.image.IImageConstant;
+import org.jowidgets.tools.layout.MigLayoutFactory;
+import org.jowidgets.tutorials.rcp3.tutorial1.common.bean.IPerson;
+import org.jowidgets.workbench.api.IViewContext;
+import org.jowidgets.workbench.tools.AbstractView;
+
+public final class PersonDetailView extends AbstractView {
+
+	public static final String ID = PersonDetailView.class.getName();
+	public static final String DEFAULT_LABEL = "Person form";
+	public static final String DEFAULT_TOOLTIP = "Shows the detail form of the person";
+	public static final IImageConstant DEFAULT_ICON = SilkIcons.USER;
+
+	public PersonDetailView(final IViewContext context, final IBeanTableModel<IPerson> model) {
+		final IContainer container = context.getContainer();
+		container.setLayout(MigLayoutFactory.growingInnerCellLayout());
+
+		final ICapApiBluePrintFactory cbpf = CapUiToolkit.bluePrintFactory();
+		final IBeanSelectionFormBluePrint formBp = cbpf.beanSelectionForm(model);
+		final List<IAttribute<Object>> attributes = model.getAttributes(MetaAttributesFilter.getInstance());
+		formBp.setBeanForm(cbpf.beanForm(IPerson.class, attributes));
+
+		container.add(formBp, MigLayoutFactory.GROWING_CELL_CONSTRAINTS);
+	}
+}
---
+++ trunk/rcp3/tsol/jowidgets6/org.jowidgets.examples.rcp3.example6/src/org/jowidgets/examples/rcp3/example6/Example6Application.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2013, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.examples.rcp3.example6;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.jowidgets.api.model.table.ITableModel;
+import org.jowidgets.api.toolkit.Toolkit;
+import org.jowidgets.api.widgets.IFrame;
+import org.jowidgets.api.widgets.blueprint.IFrameBluePrint;
+import org.jowidgets.common.application.IApplication;
+import org.jowidgets.common.application.IApplicationLifecycle;
+import org.jowidgets.common.types.Dimension;
+import org.jowidgets.common.widgets.layout.MigLayoutDescriptor;
+import org.jowidgets.tools.layout.MigLayoutFactory;
+import org.jowidgets.tools.widgets.blueprint.BPF;
+
+public final class Example6Application implements IApplication {
+
+	private static final Random RANDOM = new Random();
+	private static final long AGE_VARIANZ = 1000 * 60 * 60 * 24 * 365 * 50;
+
+	@Override
+	public void start(final IApplicationLifecycle lifecycle) {
+
+		final IFrameBluePrint frameBp = BPF.frame();
+		frameBp.setSize(new Dimension(800, 600)).setTitle("Hello World");
+		final IFrame frame = Toolkit.createRootFrame(frameBp, lifecycle);
+		frame.setLayout(new MigLayoutDescriptor("0[grow, 0::]0", "0[grow, 0::]0"));
+
+		final ITableModel tableModel = createTableModel();
+		frame.add(BPF.table(tableModel), MigLayoutFactory.GROWING_CELL_CONSTRAINTS);
+
+		frame.setVisible(true);
+	}
+
+	private static ITableModel createTableModel() {
+		final BeanTableModel<Person> result = new BeanTableModel<>(new PersonTableRenderer());
+
+		final Person person1 = new Person();
+		person1.setName("Ben Ebelt");
+		person1.setGender(Gender.MALE);
+		person1.setDayOfBirth(createDate());
+		result.addBean(person1, true);
+
+		final Person person2 = new Person();
+		person2.setName("Dieter Rasse");
+		person2.setGender(Gender.MALE);
+		person2.setDayOfBirth(createDate());
+		result.addBean(person2, false);
+
+		final Person person3 = new Person();
+		person3.setName("Klara Fall");
+		person3.setGender(Gender.FEMALE);
+		person3.setDayOfBirth(createDate());
+		result.addBean(person3, false);
+
+		return result;
+	}
+
+	private static Date createDate() {
+		return new Date((long) (System.currentTimeMillis() - (RANDOM.nextDouble() * AGE_VARIANZ)));
+	}
+}
