605af1cec82d2e2eb4db2e242caf000bde2fe4b3
herr.grossmann <herr.grossmann@gmx.de>
2014-04-08 08:54:27 +0000

    removed content

--- rcp3/tutorial/jowidgets1/org.jowidgets.examples.rcp3.example1/src/org/jowidgets/examples/rcp3/example1/HelloWorldApplication.java
+++ rcp3/tutorial/jowidgets1/org.jowidgets.examples.rcp3.example1/src/org/jowidgets/examples/rcp3/example1/HelloWorldApplication.java
@@ -27,51 +27,14 @@
  */
 package org.jowidgets.examples.rcp3.example1;
 
-import org.jowidgets.api.toolkit.Toolkit;
-import org.jowidgets.api.widgets.IButton;
-import org.jowidgets.api.widgets.IFrame;
-import org.jowidgets.api.widgets.blueprint.IButtonBluePrint;
-import org.jowidgets.api.widgets.blueprint.IFrameBluePrint;
 import org.jowidgets.common.application.IApplication;
 import org.jowidgets.common.application.IApplicationLifecycle;
-import org.jowidgets.common.types.Dimension;
-import org.jowidgets.common.widgets.controller.IActionListener;
-import org.jowidgets.common.widgets.layout.MigLayoutDescriptor;
-import org.jowidgets.tools.widgets.blueprint.BPF;
 
 public final class HelloWorldApplication implements IApplication {
 
 	@Override
 	public void start(final IApplicationLifecycle lifecycle) {
-
-		//Create a frame BluePrint with help of the BluePrintFactory (BPF)
-		final IFrameBluePrint frameBp = BPF.frame();
-		frameBp.setSize(new Dimension(400, 300)).setTitle("Hello World");
-
-		//Create a frame with help of the Toolkit and BluePrint. This convenience
-		//method finishes the ApplicationLifecycle when the root frame will be closed.
-		final IFrame frame = Toolkit.createRootFrame(frameBp, lifecycle);
-
-		//Use a simple MigLayout with one column and one row for the frame (a frame is a container also)
-		frame.setLayout(new MigLayoutDescriptor("[]", "[]"));
-
-		//Create a button BluePrint with help of the BluePrintFactory (BPF)
-		final IButtonBluePrint buttonBp = BPF.button().setText("Hello World");
-
-		//Add the button defined by the BluePrint to the frame
-		final IButton button = frame.add(buttonBp);
-
-		//Add an ActionListener to the button
-		button.addActionListener(new IActionListener() {
-			@Override
-			public void actionPerformed() {
-				//CHECKSTYLE:OFF
-				System.out.println("Hello World");
-				//CHECKSTYLE:ON
-			}
-		});
-
-		//set the root frame visible
-		frame.setVisible(true);
+		//TODO add hello world content here
+		lifecycle.finish();
 	}
 }
